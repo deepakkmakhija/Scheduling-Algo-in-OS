6be44832f8e9d9408a53e6d53fa36082f5c3c4e1
deepakkmakhija <62255654+deepakkmakhija@users.noreply.github.com>
2020-03-22 19:19:41 +0530

    Update makhija.c

--- makhija.c
+++ makhija.c
@@ -10,3 +10,19 @@ struct job{
 
 int n, fc=0, sc=0, mc=0;
 int quanta;
+void roundRobin(){
+	int time= m[0].at, mark=0, cc=0, i, rc;
+	while(time!=120 && cc!=mc){
+		for(i=0; i<=mark; i++){
+			if(m[i].rbt > quanta){
+				time += quanta;
+				m[i].rbt -= quanta;
+			}
+			else if(m[i].rbt <=quanta && m[i].rbt !=0){
+				time += m[i].rbt;
+				m[i].rbt =0;
+				m[i].cmpt = time;
+				cc++;
+			}
+			else;
+		}
